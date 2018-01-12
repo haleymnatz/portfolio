b67f2377ee824d9bc100b8a3c0d6faa37e7f5a9a
haleymnatz <haleymnatz@gmail.com>
2018-01-12 08:53:25 -0500

    About page completed, responsive styling started

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,14 @@
-// function getNav(){
-//   $(".nav-placeholder").load('../html/nav.html');
-// }
+function scrollToId(){
+  $('.nav-link').click(function(e) {
+    e.preventDefault();
+    var divId = '#' + this.dataset.attr;
 
-function openContact(){
-  $('#contact-link').click(function(){
-    $('main').load('../html/contact.html');
-  });
-}
-
-function openHome(){
-  $('#home-link').click(function(){
-    $('main').load('../html/index.html #home');
-  });
-}
-
-function openWork(){
-  $('#work-link').click(function(){
-    $('main').load('../html/work.html', function(){
-      // alert("Success!");
-      // history.pushState({"html":response.html,"pageTitle":response.pageTitle}, '', 'work');
-    });
-  });
-}
-
-// function getWork(){
-//   $('#work-link').click(function(){
-//     $.get('/html/work.html', function(data) {
-//
-//     });
-//   });
-// }
-
-function openAbout(){
-  $('#about-link').click(function(){
-    $('main').load('../html/about.html');
+  	$('body, html').animate({
+  		scrollTop: $(divId).offset().top
+  	}, 500);
   });
 }
 
 $(function(){
-  // getNav();
-  // openContact();
-  // openHome();
-  // openWork();
-  // openAbout();
+  scrollToId();
 });
