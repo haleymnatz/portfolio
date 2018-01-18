a9d4d962cae9ac6d6c856a15311f8abbca52d446
haleymnatz <haleymnatz@gmail.com>
2018-01-18 11:21:32 -0500

    Finish responsive styling, site MVP completed

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,13 @@ function scrollToId(){
     e.preventDefault();
     var divId = '#' + this.dataset.attr;
 
+    if ($(window).width() <= 730) {
+      var scrollHere = $(divId).offset().top - 50;
+    } else {
+      scrollHere =  $(divId).offset().top;
+    }
   	$('body, html').animate({
-  		scrollTop: $(divId).offset().top
+  		scrollTop: scrollHere
   	}, 500);
   });
 }
@@ -23,7 +28,31 @@ function darkenContent() {
   $('#site-cover-div').toggleClass('site-cover', checked);
 }
 
+function revealInfoDirectClick() {
+  if ($(window).width() <= 730) {
+    $('.project').click(function() {
+      $(this).toggleClass('open');
+    });
+  }
+}
+
+function revealInfoFromDots() {
+  $('.fa-ellipsis-v').click(function(){
+    $(this).parent().parent().siblings('.project').toggleClass('open');
+  });
+}
+
+function closeMobileMenu() {
+  $('.nav-lists a').click(function(){
+    $('#nav-trigger').prop('checked', false);
+    darkenContent();
+  });
+}
+
 $(function(){
   scrollToId();
   checkSize();
+  revealInfoFromDots();
+  revealInfoDirectClick();
+  closeMobileMenu();
 });
